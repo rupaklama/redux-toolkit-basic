cc619306a25b4f57e2386f8f8a48187b93f01fc6
Rupak Lama <rlama@uptopcorp.com>
2021-01-19 22:05:05 -0500

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,20 @@
+// app dir - contains the store configuration
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
+// import our reducer from the slice
+import counterReducer from '../features/counter/counterSlice';
+import todosReducer from '../features/todos/todosSlice';
+import postsReducer from '../features/posts/postsSlice';
+
+// Use 'configureStore' function to create the store with Object options. 
+// It will be directly used as the root reducer for the store
+// by passing this object to the Redux combineReducers utility.
 export default configureStore({
   reducer: {
+    // passing each of our Reducers as key/value properties to create State Object in Redux Store
     counter: counterReducer,
+    todos: todosReducer,
+    posts: postsReducer
   },
+  // devTools: false
 });
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,27 +1,48 @@
+// features dir contains all the App Features with its related files & STORE object
+
 import { createSlice } from '@reduxjs/toolkit';
 
+// Slice is an object that contains a Reducer function as a field name - key
+// basically, a slice is a store part responsible for a SINGLE FEATURE - global state object
 export const counterSlice = createSlice({
+  // A SLICE name, used in action types in the store with Action Creator- type :"counter/increment"
+  // 'counter' state object in our store
   name: 'counter',
+
+  // On regular reducer: const counterReducer = (state, action) => {switch{}}
+  // The initial state for the reducer - state from above
   initialState: {
     value: 0,
   },
+
+  // reducers - key/value pair instead of switch statements
+  // An object of "case reducers". Key names will be used to generate actions.
   reducers: {
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
+
+    // to update 'counter' State object's value property in the STORE
     increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1;
     },
     decrement: state => {
       state.value -= 1;
     },
+    
+     // NOTE: first arg - 'state' is the reference to the initial State of the reducer
+    // Second arg - 'action' is Action object which has Type & Payload, can be destructure
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
   },
 });
 
+// Note: Slice will automatically/dynamically give us Action Types - Action Creators
+// Note: These are NOT reducers functions above. 
+// These are Action Creators with the same name as reducers. 
+// you can change name by doing - increment: incrementAction
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -37,6 +58,10 @@ export const incrementAsync = amount => dispatch => {
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = state => state.counter.value;
+export const selectCount = state => state.counter.value; 
+// Accessing 'counter' object of Global State in Store
+// To make the Counter State accessible & reusable everywhere in other components for DRY
 
+// It is a convention to export reducer as a default export
+// Exporting this counterSlice as counterReducer
 export default counterSlice.reducer;
